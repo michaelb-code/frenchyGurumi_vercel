038fc201effa3ced17da984aede181bfa2241ab2
michaelb-code <biganzolimichael@gmail.com>
2025-03-07 19:03:21 +0100

    navbar4

--- front/redux/reducers/store.js
+++
@@ -1,7 +0,0 @@
-//import de la fonction configureStore de la librairie @reduxjs/toolkit
-import { configureStore } from '@reduxjs/toolkit';
-
-//export de la fonction configureStore
-export default configureStore({
-    reducer: {}
-});
--- front/src/index.js
+++ front/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import store from './redux/reducers/store';
+
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
