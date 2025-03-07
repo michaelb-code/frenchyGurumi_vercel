a395168d71285f507971e67569e7597c55aab9c8
michaelb-code <biganzolimichael@gmail.com>
2025-03-07 19:33:33 +0100

    navbar pbl

--- front/src/index.js
+++ front/src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import store from './src/redux/reducers/store';
+import store from '../src/redux/reducers/store.js';
 
 import { Provider } from 'react-redux';
 
