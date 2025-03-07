14bb6eda2e2a9ad62cb9240cc92ecbd8e4505710
michaelb-code <biganzolimichael@gmail.com>
2025-03-07 19:12:34 +0100

    navebar

--- front/src/index.js
+++ front/src/index.js
@@ -13,7 +13,7 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter> 
     <AuthProvider>
     <Provider store={store}>
     <App />
