5f938c2df79252257f004a2d884a59bb74750a40
michaelb-code <biganzolimichael@gmail.com>
2025-03-08 00:10:41 +0100

    correction api

--- front/src/constant/api.js
+++ front/src/constant/api.js
@@ -1,25 +1,25 @@
 const URL = {
 //ARTICLE
-    FETCH_ARTICLES : "frenchy-gurumi-vercel-oauk.vercel.app/api/article/get",
-    FETCH_ARTICLE : "frenchy-gurumi-vercel-oauk.vercel.app/api/article/get",
-    CREATE_ARTICLE : "frenchy-gurumi-vercel-oauk.vercel.app/api/article/create",
-    UPDATE_ARTICLE : "frenchy-gurumi-vercel-oauk.vercel.app/api/article/update",
-    DELETE_ARTICLE : "frenchy-gurumi-vercel-oauk.vercel.app/api/article/delete",
+    FETCH_ARTICLES : "https://frenchy-gurumi-vercel-oauk.vercel.app/api/article/get",
+    FETCH_ARTICLE : "https://frenchy-gurumi-vercel-oauk.vercel.app/api/article/get",
+    CREATE_ARTICLE : "https://frenchy-gurumi-vercel-oauk.vercel.app/api/article/create",
+    UPDATE_ARTICLE : "https://frenchy-gurumi-vercel-oauk.vercel.app/api/article/update",
+    DELETE_ARTICLE : "https://frenchy-gurumi-vercel-oauk.vercel.app/api/article/delete",
 
 //USER
-    FETCH_USER : "frenchy-gurumi-vercel-oauk.vercel.app/api/user/get",
-    FETCH_USER_BY_ID : "frenchy-gurumi-vercel-oauk.vercel.app/api/user/get",
-    CREATE_USER : "frenchy-gurumi-vercel-oauk.vercel.app/api/user/signup",
-    SIGNIN : "frenchy-gurumi-vercel-oauk.vercel.app/api/user/signin",
-    UPDATE_USER : "frenchy-gurumi-vercel-oauk.vercel.app/api/user/update",
-    DELETE_USER : "frenchy-gurumi-vercel-oauk.vercel.app/api/user/delete",
+    FETCH_USER : "https://frenchy-gurumi-vercel-oauk.vercel.app/api/user/get",
+    FETCH_USER_BY_ID : "https://frenchy-gurumi-vercel-oauk.vercel.app/api/user/get",
+    CREATE_USER : "https://frenchy-gurumi-vercel-oauk.vercel.app/api/user/signup",
+    SIGNIN : "https://frenchy-gurumi-vercel-oauk.vercel.app/api/user/signin",
+    UPDATE_USER : "https://frenchy-gurumi-vercel-oauk.vercel.app/api/user/update",
+    DELETE_USER : "https://frenchy-gurumi-vercel-oauk.vercel.app/api/user/delete",
 
 //AVIS
-    FETCH_AVIS : "frenchy-gurumi-vercel-oauk.vercel.app/api/avis/get",
-    FETCH_AVIS_BY_ID : "frenchy-gurumi-vercel-oauk.vercel.app/api/avis/get",
-    CREATE_AVIS : "frenchy-gurumi-vercel-oauk.vercel.app/api/avis/create",
-    UPDATE_AVIS : "frenchy-gurumi-vercel-oauk.vercel.app/api/avis/update",
-    DELETE_AVIS : "frenchy-gurumi-vercel-oauk.vercel.app/api/avis/delete"
+    FETCH_AVIS : "https://frenchy-gurumi-vercel-oauk.vercel.app/api/avis/get",
+    FETCH_AVIS_BY_ID : "https://frenchy-gurumi-vercel-oauk.vercel.app/api/avis/get",
+    CREATE_AVIS : "https://frenchy-gurumi-vercel-oauk.vercel.app/api/avis/create",
+    UPDATE_AVIS : "https://frenchy-gurumi-vercel-oauk.vercel.app/api/avis/update",
+    DELETE_AVIS : "https://frenchy-gurumi-vercel-oauk.vercel.app/api/avis/delete"
 
 }
 
