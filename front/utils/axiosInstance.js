4171edf5b4089f988c0bf7cdd013c010e2f3b661
michaelb-code <biganzolimichael@gmail.com>
2025-03-09 18:14:46 +0100

    formdata2

--- front/utils/axiosInstance.js
+++ front/utils/axiosInstance.js
@@ -3,13 +3,13 @@ import axios from "axios";
 const API_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000"
-    : "https://frenchy-gurumi-vercel.vercel.app/";
+    : "https://frenchy-gurumi-vercel-oauk.vercel.app/";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
   withCredentials: true,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
 
