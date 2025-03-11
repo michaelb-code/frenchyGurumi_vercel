a62dbdfc1c5553a3f4602823f9c8a7407ef7b755
michaelb-code <biganzolimichael@gmail.com>
2025-03-11 12:12:56 +0100

    slide3.1

--- front/src/components/Slider.jsx
+++ front/src/components/Slider.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Carousel} from 'react-responsive-carousel'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-
+import './Slider.css';
 
 
 function Slider() {
@@ -27,7 +27,7 @@ function Slider() {
       {datas.map((data) => (
         <div key={data.id}>
           <img src={data.image} alt={data.alt} />
-          
+
         </div>
       ))}
     </Carousel>
