1c6e53845ac07bf90288e0de4ddd7065dd702293
michaelb-code <biganzolimichael@gmail.com>
2025-03-11 02:16:52 +0100

    slide

--- front/src/components/Slider.jsx
+++ front/src/components/Slider.jsx
@@ -5,12 +5,13 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 
 function Slider() {
+  
   return (
     
         <Carousel>
-            <div>Slider 1</div>
-            <div>Slider 2</div>
-            <div>Slider 3</div>
+            <div><img src={'/photosSlide/photoslide1.jpg'} alt="Slider 1" /></div>
+            <div><img src={'/photosSlide/photoslide2.jpg'} alt="Slider 2" /></div>
+            <div><img src={'/photosSlide/photoslide3.jpg'} alt="Slider 3" /></div>
         </Carousel>
     
   )
